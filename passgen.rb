58158f5e743dca8d078be0098aff768288306987
Frikker <stolpnikk.97@gmail.com>
2016-09-23 20:38:55 +0300

    ver. 1.1: reworked re-obtain the password

--- passgen.rb
+++ passgen.rb
@@ -3,11 +3,18 @@ require './passwordgenerator.rb'
 temp = nil
 G = PasswordGenerator.new
 
-while temp != 2 || temp == nil
+while temp != 2
   G.set_args
   puts "Ваши пароли:
 #{G.get_password_list}
 Получить пароли с другими параметрами?
 1. Да 2. Нет"
   temp = gets.chomp.to_i
+  while temp != 1
+    puts 'Вы ввели неверное значение. Повторите ввод.'
+    temp = gets.chomp.to_i
+    if temp == 2
+      exit
+    end
+  end
 end
